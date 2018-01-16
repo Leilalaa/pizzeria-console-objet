95a95ae33d98b2260d7e1b31478dfa85ee8b0c47
LeilaLaa <aaleila19@gmail.com>
2018-01-16 16:50:21 +0100

    Ajout JPA a Pizzeria

--- src/fr/pizzeria/dao/PizzaDaoJdbc.java
+++ src/fr/pizzeria/dao/PizzaDaoJdbc.java
@@ -1,138 +1,88 @@
 package fr.pizzeria.dao;
 
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.SQLException;
-
-import fr.pizzeria.model.CategoriePizza;
 import fr.pizzeria.model.Pizza;
 
-import java.util.ResourceBundle;
+
 
 
 public class PizzaDaoJdbc implements IPizzaDao {
 
-	ResourceBundle props = ResourceBundle.getBundle("jdbc");
-	//String driverName = props.getString("jdbc.driverClassName");
-
-	String url = props.getString("db.url");
-	String utilisateur = props.getString("db.username");
-	String motDePasse = props.getString("db.password");
-	Connection connection = null;
+	EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_pizza");
+	EntityManager em = entityManagerFactory.createEntityManager();
 
 	public PizzaDaoJdbc() {
-
-		try {
-
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(url, utilisateur, motDePasse);
-
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
 
 	}
 
 	public ArrayList<Pizza> findAllPizzas() {
 
 		ArrayList<Pizza> pizzas = new ArrayList();
-		
-		try {
-		Statement statement = connection.createStatement();
 
-		ResultSet resultats = statement.executeQuery("SELECT * FROM btoapuzaw.pizza");
+		TypedQuery<Pizza> query = em.createQuery("from Pizza", Pizza.class);
 
-		while (resultats.next()) {
+		for (Pizza p : query.getResultList()) {
 
-			String code = resultats.getString("id");
-			String nom = resultats.getString("pizza_name");
-			Double prix = resultats.getDouble("price");
-			String categorieStr = resultats.getString("category").toUpperCase();
-			CategoriePizza categorie = CategoriePizza.valueOf(categorieStr);
-			
-			Pizza p = new Pizza(code, nom, prix, categorie);
 			pizzas.add(p);
-			
-			
-		}
-		statement.close();
-		resultats.close();
 
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		
+
 		return pizzas;
-
 
 	}
 
 	public boolean updatePizza(String choixCode, Pizza pizza) {
-		
-		try {
-			
-		PreparedStatement updatePizzaSt = connection.prepareStatement("UPDATE btoapuzaw.pizza SET ID=?, PIZZA_NAME=?, PRICE=?, CATEGORY=? WHERE ID=?"); 
-		
-		updatePizzaSt.setString(1,pizza.getCode()); 
-		updatePizzaSt.setString(2,pizza.getNom()); 
-		updatePizzaSt.setDouble(3,pizza.getPrix());
-		updatePizzaSt.setString(4,pizza.getCat().toString());
-		updatePizzaSt.setString(5,choixCode);
-		updatePizzaSt.executeUpdate();
-		updatePizzaSt.close();
 
+			
+	
+			TypedQuery<Pizza> query = em.createQuery("from Pizza WHERE code=:target", Pizza.class);
+			query.setParameter("target", choixCode);
+			Pizza p = query.getResultList().get(0);
+			EntityTransaction et = em.getTransaction(); 
+			et.begin();
+			
+			p.setNom(pizza.getNom());
+			p.setPrix(pizza.getPrix());
+			p.setCat(pizza.getCat());
+			p.setCode(pizza.getCode());
+			
+			et.commit();
 		
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
+	
 		return false;
 	}
 
 	public boolean saveNewPizza(Pizza pizza) throws SavePizzaException {
-
-		PreparedStatement savePizzaSt;
-		try {
-			
-			savePizzaSt = connection.prepareStatement("INSERT INTO btoapuzaw.pizza VALUES(?,?,?,?)"); 
-			
-			savePizzaSt.setString(1,pizza.getCode()); 
-			savePizzaSt.setString(2,pizza.getNom()); 
-			savePizzaSt.setDouble(3,pizza.getPrix());
-			savePizzaSt.setString(4,pizza.getCat().toString());
-			savePizzaSt.executeUpdate();
-			savePizzaSt.close();
-
-			
-			
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return false;
+		
+		EntityTransaction et = em.getTransaction(); 
+		et.begin();
+		em.persist(pizza);
+		et.commit();
+		
+		return false;
 	}
 
 	public boolean deletePizza(String choixCode) {
-		
-		try {
-			PreparedStatement deletePizzaSt = connection.prepareStatement("DELETE FROM btoapuzaw.pizza WHERE ID=?"); 
-			
-			deletePizzaSt.setString(1,choixCode);  
-			deletePizzaSt.executeUpdate();
-			deletePizzaSt.close();
 
-			
-			
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		TypedQuery<Pizza> query = em.createQuery("from Pizza WHERE code=:target", Pizza.class);
+		query.setParameter("target", choixCode);
+		Pizza p = query.getResultList().get(0);
+		EntityTransaction et = em.getTransaction(); 
+		et.begin();
+		
+		em.remove(p);
+		
+		et.commit();
 		return false;
 	}
 
